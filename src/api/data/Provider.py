b7eaa5acc921c4ec7b37a00d51c60052a054c39c
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-04-01 16:47:44 +0200

    [PROVIDERS] Data providers

---
+++ src/api/data/Provider.py
@@ -0,0 +1,28 @@
+#  Mahjopi-IaC - API
+#  Infrastructure as Code project, automatic machine and network deployment
+#  Copyright (c), MahjoPi, 2022.
+#  This code belongs exclusively to its authors, use, redistribution or reproduction
+#  forbidden except with authorization from the authors.
+
+#Create an abstract class for data providers
+
+class Provider:
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
+
+    def get_deployed_instances(self):
+        """
+        Returns a list of instances that are deployed on the provider
+        :return: list[Machine]
+        """
+        pass
+
+    def get_deployed_networks(self):
+        """
+        Returns a list of networks that are deployed on the provider
+        :return: list[Network]
+        """
+        pass
