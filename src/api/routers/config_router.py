6b60877a563a5b14012a90c5ea41209b07f0c947
Alexis LEBEL <lebalexis@gmail.com>
2022-04-04 17:09:36 +0200

    [MISC] Fixed copyright

--- src/api/routers/config_router.py
+++ src/api/routers/config_router.py
@@ -3,15 +3,10 @@
 #  Copyright (c), MahjoPi, 2022.
 #  This code belongs exclusively to its authors, use, redistribution or reproduction
 #  forbidden except with authorization from the authors.
-#  Mahjopi-IaC - API
-#  Infrastructure as Code project, automatic machine and network deployment
-#  Copyright (c), MahjoPi, 2022.
-#  This code belongs exclusively to its authors, use, redistribution or reproduction
-#  forbidden except with authorization from the authors.
+
 from fastapi import APIRouter
 
 from data.storage import TerraformStorage as storage
-from models.Machine import Machine
 from models.TerraformConfig import TerraformConfig
 
 router = APIRouter(
