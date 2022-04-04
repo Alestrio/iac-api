0d2b182ce377621af7db760bb36802cfab717d86
Alexis LEBEL <lebalexis@gmail.com>
2022-04-04 16:34:36 +0200

    [MODELS] Updated models

--- src/api/models/Disk.py
+++ src/api/models/Disk.py
@@ -3,6 +3,8 @@
 #  Copyright (c), MahjoPi, 2022.
 #  This code belongs exclusively to its authors, use, redistribution or reproduction
 #  forbidden except with authorization from the authors.
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -10,11 +12,15 @@ class Disk(BaseModel):
     """
     That class represents a disk. It is independent of the provider.
     """
-    id: str
-    provider: str
-    type: str
-    subtype: str
-    size: int
-    region: str
-    zone: str
-    name: str
+    id: Optional[str]
+    provider: str = "aws"
+    aws_type: str = "gp2"
+    aws_subtype: str = "standard"
+    gcp_type: str = "pd-standard"
+    gcp_subtype: str = "standard"
+    size: int = 10  # GB
+    aws_region: str = "eu-central-1"
+    gcp_region: str = "europe-west1"
+    aws_zone: str = "eu-central-1a"
+    gcp_zone: str = "europe-west1-b"
+    name: str = "disk"
--- src/api/models/Machine.py
+++ src/api/models/Machine.py
@@ -3,44 +3,38 @@
 #  Copyright (c), MahjoPi, 2022.
 #  This code belongs exclusively to its authors, use, redistribution or reproduction
 #  forbidden except with authorization from the authors.
+from typing import Optional
+
+from pydantic import BaseModel
+
 from models.Disk import Disk
 
 
-class Machine:
+class Machine(BaseModel):
     """
-    Machine class
+    :param disks: List of Disk objects, disks to be attached to the machine \
+    :param gcp_zone: Zone of the GCP machine, default is us-central1-a
+    :param aws_zone: Zone of the AWS machine, default is eu-west-3
+    :param gcp_network: Network of the GCP machine, default is "default"
+    :param aws_network: Network of the AWS machine, default is empty
+    :param gcp_machine_type: Machine type, default is e2-micro
+    :param aws_machine_type: Machine type, default is t2.micro
+    :param gcp_machine_image: The image to use for the GCP machine, default is debian-10-buster-v20200101
+    :param aws_machine_image: The image to use for the AWS machine, default is a debian 9 stretch image
+    :param cpu: The number of CPUs to allocate to the machine, default is 0, using the machine type
+    :param memory: The amount of memory to allocate to the machine, default is 0, using the machine type
+    :param machine_name: The name of the machine, default is "machine"
     """
-    def __init__(self, disks: list[Disk], gcp_zone="us-central1-a", aws_zone="eu-west-3", gcp_network="default",
-                 aws_network="", gcp_machine_type="e2-micro", aws_machine_type="t2.micro",
-                 gcp_machine_image="debian-10-buster-v20200101", aws_machine_image="ami-045fa58af83eb0ff4",
-                 cpu=0, memory=0, machine_name="machine",):
-        """
-        Machine class constructor
 
-        :param disks: List of Disk objects, disks to be attached to the machine \
-        :param gcp_zone: Zone of the GCP machine, default is us-central1-a
-        :param aws_zone: Zone of the AWS machine, default is eu-west-3
-        :param gcp_network: Network of the GCP machine, default is "default"
-        :param aws_network: Network of the AWS machine, default is empty
-        :param gcp_machine_type: Machine type, default is e2-micro
-        :param aws_machine_type: Machine type, default is t2.micro
-        :param gcp_machine_image: The image to use for the GCP machine, default is debian-10-buster-v20200101
-        :param aws_machine_image: The image to use for the AWS machine, default is a debian 9 stretch image
-        :param cpu: The number of CPUs to allocate to the machine, default is 0, using the machine type
-        :param memory: The amount of memory to allocate to the machine, default is 0, using the machine type
-        :param machine_name: The name of the machine, default is "machine"
-        """
-        if cpu == 0 or memory == 0:
-            self.gcp_type = gcp_machine_type
-            self.aws_type = aws_machine_type
-        else:
-            self.cpu = cpu
-            self.memory = memory
-        self.gcp_machine_image = gcp_machine_image
-        self.aws_machine_image = aws_machine_image
-        self.name = machine_name
-        self.gcp_zone = gcp_zone
-        self.aws_zone = aws_zone
-        self.gcp_network = gcp_network
-        self.aws_network = aws_network
-        self.disks = disks
+    name: str = "machine"
+    cpu: int = 0
+    memory: int = 0
+    gcp_type: Optional[str] = "e2-micro"
+    aws_type: Optional[str] = "t2.micro"
+    gcp_machine_image: str = "debian-10-buster-v20190923"
+    aws_machine_image: str = "ami-0f9c9d7f2b6c8f9d6"
+    gcp_zone: str = "europe-west1-b"
+    aws_zone: str = "eu-west-1a"
+    gcp_network: str = "default"
+    aws_network: str = "default"
+    disks: list[Disk]
