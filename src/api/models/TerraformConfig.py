d3353af957639cd3d2515e8d7f3e6bf5fe2f521d
Alexis LEBEL <lebalexis@gmail.com>
2022-04-07 15:03:10 +0200

    [MODELS] Adapted Terraform config

--- src/api/models/TerraformConfig.py
+++ src/api/models/TerraformConfig.py
@@ -14,7 +14,8 @@ from models.Network.Network import Network
 class TerraformConfig(BaseModel):
     name: str = os.urandom(8).hex()
     project_id: str
-    region: str = "eu-west-1"
+    gcp_region: str = "europe-west1"
+    aws_region: str = "eu-west-1"
     ssh_user: str = "ubuntu"
     private_key_name: str = "sample-key"
     machines: list[Machine]
