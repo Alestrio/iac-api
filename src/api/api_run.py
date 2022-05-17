f819f23be52512536b8f922aa0d94b3e7f26e408
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-05-17 17:15:18 +0200

    [GENERAL] Permitted run from cli

--- src/api/api_run.py
+++ src/api/api_run.py
@@ -5,6 +5,11 @@
 #  forbidden except with authorization from the authors.
 
 import uvicorn
+import os
+import sys
+
+root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+sys.path.append(root_folder)
 from src.api import app
 
 if __name__ == '__main__':
