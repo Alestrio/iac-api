9aa12bc4e7c6aad8ef86d9f0cb4a8d1f3d3e68fb
Alexis LEBEL <alexis.lebel@mahjopi.com>
2022-05-19 16:08:55 +0200

    [API] Made debug mode true

--- src/api/api_run.py
+++ src/api/api_run.py
@@ -13,4 +13,4 @@ sys.path.append(root_folder)
 from src.api import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("src.api:app", host="0.0.0.0", port=8000, reload=True)
